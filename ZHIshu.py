266c2767823ae02db39062a93d5417b73bc35800
243459529 <243459529@qq.com>
2020-10-15 20:57:56 +0800

    enter

--- ZHIshu.py
+++ ZHIshu.py
@@ -8,15 +8,19 @@ def exponential_func(x, a=3):
 
 
 X=np.linspace(-3, 3, 30) 
-Y=[exponential_func(x) for x in X] 
-
+Y1=[exponential_func(x,3) for x in X] 
+Y2=[exponential_func(x,0.334) for x in X]
+Y3=[exponential_func(x,1) for x in X]
 
 plt.title('a^x的图像')
-plt.xlabel('t')
+plt.xlabel('                                                                         t')
 plt.ylabel('x(t)')
+
 
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
-plt.plot(X, Y) 
+plt.plot(X, Y1) 
+plt.plot(X, Y2) 
+plt.plot(X,Y3)
 plt.show()
--- sin.py
+++ sin.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 x=np.arange(-15,3*np.pi,0.01)
-y=np.sin(x)
+y=np.sin(x+math.pi)
 
-plt.title('sin(t)的图像')
-plt.xlabel('t')
-plt.ylabel('x(t)')
+plt.title('具有初始相位的sin(t)的图像')
+plt.xlabel('                                                                          t')
+plt.ylabel('x(t)=Asin(ωt+φ)')
 
 plt.rcParams['font.sans-serif']=['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
